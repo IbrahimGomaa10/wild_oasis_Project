6e73c9107d7f99a87ed654519f802566d8eacd81
IbrahimGomaa10 <ibrahim.gomaa1013@gmail.com>
2024-06-10 17:54:30 +0300

    after adding log out and sign up features by creating new user account by admin to employees to be accessed

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -1,13 +1,31 @@
 import supabase from "./supabase";
+
+export async function signup({ fullName, password, email }) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        fullName,
+        avatar: "",
+      },
+    },
+  });
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+}
+
 export async function login({ email, password }) {
-  let { data, error } = await supabase.auth.signInWithPassword({
+  const { data, error } = await supabase.auth.signInWithPassword({
     email,
     password,
   });
   if (error) {
     throw new Error(error.message);
   }
-  console.log(data);
 
   return data;
 }
@@ -20,4 +38,9 @@ export async function getCurrentUser() {
 
   if (error) throw new Error(error.message);
   return data?.user;
+}
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+  if (error) throw new Error(error.message);
 }
